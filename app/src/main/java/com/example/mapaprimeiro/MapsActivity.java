3de5641c7d80657577ce6c2499b2f25290b11b89
lucas <lucassodrealves4@gmail.com>
2021-08-25 21:50:01 -0300

    Com Modelo de Local e Opções e Cliques

--- app/src/main/java/com/example/mapaprimeiro/MapsActivity.java
+++ app/src/main/java/com/example/mapaprimeiro/MapsActivity.java
@@ -1,13 +1,17 @@
 package com.example.mapaprimeiro;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.mapaprimeiro.databinding.ActivityMapsBinding;
@@ -16,6 +20,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    double latitude, longitude;
+    String local;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +44,91 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
+     * / **
+     * * Manipula o mapa quando disponível.
+     * * Este retorno de chamada é acionado quando o mapa está pronto para ser usado.
+     * * Aqui é onde podemos adicionar marcadores ou linhas, adicionar ouvintes ou mover a câmera. Nesse caso,
+     * * acabamos de adicionar um marcador perto de Sydney, Austrália.
+     * * Se o Google Play Services não estiver instalado no dispositivo, o usuário será solicitado a instalar
+     * * dentro do SupportMapFragment. Este método só será acionado quando o usuário tiver
+     * * instalou o Google Play Services e voltou ao aplicativo.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+//normal - a visualização do roteiro padrão. Este é o tipo de mapa padrão.
+//satellite - exibe imagens de satélite do Google Terra.
+//hybrid - exibe uma mistura de visualizações normais e de satélite.
+//terrain - exibe um mapa físico com base nas informações do terreno.
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        latitude = -22.981150116303127;
+        longitude = -45.794577476283365;
+        local = "Sítio!";
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng position = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions()
+                .position(position)
+                .title(local)
+                .snippet("O do Pica Pau Amarelo")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+        mMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.moray)));
+
+        //A Acrescentar:Outras Opções de Marcação
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 12.5f));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                double latitude, longitude;
+
+                latitude = latLng.latitude;
+
+                longitude = latLng.longitude;
+
+                Toast.makeText(MapsActivity.this, "Clique Curto-Latitude:" + latitude + " " + "e Longitude:" + longitude, Toast.LENGTH_SHORT).show();
+                mMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title("Fez um Pequeno Clique")
+                        .snippet("Gera-se assim um ícone de uma Invenção Milenar")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.locomotiva))
+                );
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+
+            }
+        });
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                double latitude, longitude;
+
+                latitude = latLng.latitude;
+                longitude = latLng.longitude;
+
+                Toast.makeText(MapsActivity.this, "Clique Longo - Latitude:" + latitude + " " + "Longitude:" + longitude, Toast.LENGTH_SHORT).show();
+                mMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title("Clique Longo")
+                        .snippet("Um Animal Noturno")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.coruja))
+                );
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
